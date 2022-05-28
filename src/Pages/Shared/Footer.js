1a0865c795f0b3bea0a5f596b4283de903216165
Meherabhossen <meherabhossen45@gmail.com>
2022-05-29 00:06:23 +0600

    fotter add

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -11,20 +11,20 @@ const Footer = () => {
                     <b>22/6 North Lalbag , Dhaka-1205 , Bangladesh</b><br />
                 </div>
                 <div className='flex'>
-                    <a href=""><PhoneIcon className='Icon'></PhoneIcon></a><b> Helpline:   +8801735776381, +8801571359201</b>
+                    <a href=""><PhoneIcon className='Icon'></PhoneIcon></a><b> Helpline:   +8801735123451, +8801574563201</b>
                 </div>
                 <div className='flex'>
-                    <a href="http://"><MailIcon className='Icon'></MailIcon></a><b> Email : foysalahamedbadsha@gmail.com</b>
+                    <a href="http://"><MailIcon className='Icon'></MailIcon></a><b> Email : meherabhossen45@gmail.com</b>
                 </div>
             </div>
             <div className='flex'>
-                <a href="https://www.facebook.com/foysalahamed.badsha">
+                <a href="https://www.google.com/">
                     <ChatAltIcon className='Icon'></ChatAltIcon></a>
-                <a href="https://i.ibb.co/x5FRY5k/131692351-1365374810482039-8592008014066332892-n.jpg"><PhotographIcon className='Icon'></PhotographIcon></a>
-                <a href="https://youtu.be/rztWlsghmuE"><PlayIcon className='Icon'></PlayIcon></a>
+                <a href="https://www.tidmanlegal.com/wp-content/uploads/sites/29/2021/03/Source-Code-source-4280758_1920.jpg"><PhotographIcon className='Icon'></PhotographIcon></a>
+                <a href="https://youtu.be/DlSBC_JNuM0"><PlayIcon className='Icon'></PlayIcon></a>
             </div>
             <div className='copyright'>
-                <h1>Copyrite <FontAwesomeIcon icon={faCopyright}></FontAwesomeIcon> 2022 MD Badsha's Website</h1>
+                <h1>Copyrite <FontAwesomeIcon icon={faCopyright}></FontAwesomeIcon> 2022 Meherabs Website Website</h1>
             </div>
         </div>
     );
